4c92cedad11ded6b21fec16bc76623ffc5449add
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-10-06 10:39:49 +0300

    done /yarn command fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-
-import authors from "./data.js";
+import React, { useState ,useEffect } from "react";
+import axios from "axios";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
@@ -9,15 +8,49 @@ import AuthorDetail from "./AuthorDetail";
 const App = () => {
   const [currentAuthor, setCurrentAuthor] = useState(null);
 
-  const selectAuthor = author => setCurrentAuthor(author);
+  const [authors, setAuthors] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("Rendering");
+    fetchAuthors();
+  }, []);
+
+  const selectAuthor = async (authorID) => {
+    setLoading(true)
+    try {
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${authorID.id}/`
+      );
+      setCurrentAuthor(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("error");  
+      }
+  };
 
   const unselectAuthor = () => setCurrentAuthor(null);
 
+  const fetchAuthors = async () => {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+        );
+      setAuthors(response.data);
+      setLoading(false);
+    } catch (e) {
+      console.log("error");
+    }
+  };
   const getContentView = () => {
     if (currentAuthor) {
       return <AuthorDetail author={currentAuthor} />;
-    } else {
-      return <AuthorList authors={authors} selectAuthor={selectAuthor} />;
+    }  else {
+      if (loading) {return <h2 > LOADING ...</h2>;
+    }
+   else {
+     return <AuthorList authors={authors} selectAuthor={selectAuthor} />;}
     }
   };
 
